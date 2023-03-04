86f81c5128c4eaaa016716ddf4a8d3218015894b
Thomas Stromberg <t+github@chainguard.dev>
2023-03-04 13:39:23 -0500

    Simplify script by downloading osquery.conf

--- osquery-detection-kit.sh
+++ osquery-detection-kit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#
 # If osquery is installed, see if osquery-detection-kit picks up anything interesting.
+#
+# NOTE: event-based queries won't work under osqueryi (interactive). For fuller
+# detection use osquery-detection-kit paired with osqueryd (daemon).
 
 if ! type -P osqueryi >/dev/null; then
     echo "skipping, requires osqueryi"
@@ -17,20 +19,14 @@ fi
 
 tmpdir=$(mktemp -d)
 cd $tmpdir || exit 1
-url=$(curl -s https://api.github.com/repos/chainguard-dev/osquery-defense-kit/releases/latest | grep -E 'browser_download_url.*odk-detection.conf' | cut -d'"' -f4)
-curl -s -LO -C - ${url}
 
-cat > osquery.conf <<EOF
-{
-  // Configure the daemon below:
-  "packs": {
-    "detection": "odk-detection.conf"
-  }
-}
-EOF
+for file in osquery.conf odk-detection.conf; do
+  url=$(curl -s https://api.github.com/repos/chainguard-dev/osquery-defense-kit/releases/latest | grep -E "browser_download_url.*${file}" | cut -d'"' -f4)
+  curl -s -LO -C - "${url}"
+done
 
 osqueryi \
     --read_max=4096000000 \
     --config_path=osquery.conf \
     --pack detection \
-    2>&1 | grep -v "_events"
+    2>&1 | grep -Ev "_events|Error reading the query pack"
