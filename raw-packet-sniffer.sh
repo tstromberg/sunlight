77ef7bc72a989289b343377a9b39f7572dc2c2ba
Thomas Stromberg <t+github@chainguard.dev>
2023-10-19 08:58:05 -0400

    raw packet sniffer: ignore systemd-networkd

--- raw-packet-sniffer.sh
+++ raw-packet-sniffer.sh
@@ -7,16 +7,23 @@ if [[ $EUID != 0 ]]; then
   exit 1
 fi
 
-for inode in $(egrep -v "^sk|\s888e\s" /proc/net/packet | awk '{ print $9 }'); do
-    proc=$(find /proc -type l -lname "socket:\[${inode}\]" -ls 2>/dev/null | egrep -o "/proc/[0-9]+" | sort -u)
+for inode in $(grep -Ev "^sk|\s888e\s" /proc/net/packet | awk '{ print $9 }'); do
+    proc=$(find /proc -type l -lname "socket:\[${inode}\]" -ls 2>/dev/null | grep -Eo "/proc/[0-9]+" | sort -u)
     if [[ "${proc}" == "" ]]; then
         echo "Unable to find pid behind socket ${inode}"
         continue
     fi
-    pid=$(basename ${proc})
-    path=$(readlink /proc/$pid/exe)
-    name=$(cat /proc/$pid/comm)
+
+    pid=$(basename "${proc}")
+    path=$(readlink "/proc/${pid}/exe")
+    name=$(cat "/proc/${pid}/comm")
+
+    # ignore known sniffers
+    if [[ "${path}" = "/usr/lib/systemd/systemd-networkd" ]]; then
+      continue
+    fi
+
     echo "Raw packet sniffer: ${name} [${pid}] at ${path}:"
-    egrep "^sk|\s${inode}" /proc/net/packet
+    grep -E "^sk|\s${inode}" /proc/net/packet
     echo ""
 done
