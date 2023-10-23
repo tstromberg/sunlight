8728b3c41073e9f02b44e95bcc24d7d359da46d2
Thomas Stromberg <t+github@chainguard.dev>
2023-10-23 11:13:16 -0400

    Fix osquery-detection-kit runner

--- osquery-detection-kit.sh
+++ osquery-detection-kit.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # If osquery is installed, see if osquery-detection-kit picks up anything interesting.
-#
-# NOTE: event-based queries won't work under osqueryi (interactive). For fuller
-# detection use osquery-detection-kit paired with osqueryd (daemon).
 
 if ! type -P osqueryi >/dev/null; then
-    echo "skipping, requires osqueryi"
+    echo "skipping, requires osqueryi (INSTALLATION HIGHLY RECOMMENDED)"
     exit 0
 fi
 
@@ -18,15 +15,21 @@ fi
 [[ $EUID != 0 ]] && echo "* WARNING: For accurate output, run $0 as uid 0"
 
 tmpdir=$(mktemp -d)
-cd $tmpdir || exit 1
+cd "${tmpdir}" || exit 1
 
-for file in osquery.conf odk-detection.conf; do
-  url=$(curl -s https://api.github.com/repos/chainguard-dev/osquery-defense-kit/releases/latest | grep -E "browser_download_url.*${file}" | cut -d'"' -f4)
-  curl -s -LO -C - "${url}"
+url=$(curl -s https://api.github.com/repos/chainguard-dev/osquery-defense-kit/releases/latest | grep -E "browser_download_url.*odk-packs.zip" | cut -d'"' -f4)
+curl -q -s -LO -C - "${url}"
+
+unzip -qq odk-packs.zip
+
+for pack in odk-detection-*.conf; do
+    echo "- Running ${pack} ..."
+    name=$(echo "${pack}" | sed -e s/"odk-detection-"/""/ -e s/"\.conf"//g)
+    echo "{ \"packs\": { \"${name}\": \"${pack}\" }}" > osquery.conf
+    osqueryi \
+        --read_max=4096000000 \
+        --config_path=osquery.conf \
+        --pack "${name}" \
+        2>&1 | grep -v "_events"
 done
 
-osqueryi \
-    --read_max=4096000000 \
-    --config_path=osquery.conf \
-    --pack detection \
-    2>&1 | grep -Ev "_events|Error reading the query pack" || exit 0
